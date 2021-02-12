7f70372b9d5f9020f8c732e08497778f5ffd364f
Pedro Marques <pedromarqueslx@gmail.com>
2021-02-12 14:31:18 +0000

    Default comment

---
+++ pjrm-plugin-api.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ *	Plugin Name: PJRM Plugin
+ *	Plugin URI: https://www.pjrmdomain.com
+ *	Description: Provides both widgets and shortcodes to help you display NY Times articles on your website.
+ *	Version: 1.0
+ *	Author: Peter Markes
+ *	Author URI: https://www.pjrmdomain.com
+ *	License: GPL2
+ *
+*/
+
+?>
